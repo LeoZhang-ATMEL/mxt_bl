17651530c3995d27ec5761f267fa537741fbe832
Leo Zhang <liang.zhang@microchip.com>
2020-05-12 16:56:03 +0800

    add GCLK4 output PIN PA10

--- bootloader/apps/i2c_bootloader/bootloader/firmware/src/config/info4_board/peripheral/port/plib_port.c
+++ bootloader/apps/i2c_bootloader/bootloader/firmware/src/config/info4_board/peripheral/port/plib_port.c
@@ -74,7 +74,9 @@ void PORT_Initialize(void)
    /************************** GROUP 0 Initialization *************************/
    PORT_REGS->GROUP[0].PORT_DIR = 0x18000000;
    PORT_REGS->GROUP[0].PORT_OUT = 0x18000000;
+   PORT_REGS->GROUP[0].PORT_PINCFG[10] = 0x1;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[5] = 0x7;
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x800000;
    PORT_REGS->GROUP[1].PORT_OUT = 0x800000;
